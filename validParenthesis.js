38b27fe6aa058fbdce69d39f1b5a81d8552a316a
Shubham karad <90271066+Shubhamkarad121197@users.noreply.github.com>
2025-02-11 17:54:28 +0530

    Add files via upload

---
+++ validParenthesis.js
@@ -0,0 +1,73 @@
+// 20. Valid Parentheses
+// Solved
+// Easy
+// Topics
+// Companies
+// Hint
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+ 
+
+// Example 1:
+
+// Input: s = "()"
+
+// Output: true
+
+// Example 2:
+
+// Input: s = "()[]{}"
+
+// Output: true
+
+// Example 3:
+
+// Input: s = "(]"
+
+// Output: false
+
+// Example 4:
+
+// Input: s = "([])"
+
+// Output: true
+
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 104
+// s consists of parentheses only '()[]{}'.
+
+
+var isValid = function(s) {
+    let stack = [];
+    let map = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    };
+
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+        if (char === '(' || char === '[' || char === '{') {
+            stack.push(char);
+        } else {
+            if (stack.length === 0 || stack.pop() !== map[char]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+};
+
+console.log(isValid('()'));
+console.log(isValid('()[]{}'));
+console.log(isValid('(]'));
+console.log(isValid('([])'));
